823d2c76e4ee49bf3cf65c276c231d6b1bd1b1ff
rizkisw <you@example.comrizki4016@gmail.com>
2021-04-07 02:36:28 +0700

    first commit

---
+++ application/views/footer.php
@@ -0,0 +1,11 @@
+				<section id="footer" class="page-section">
+					<footer id="footer-section" class="page-section pt-30 pb-30 center-xs">
+						<div class="copyright fw300 mb-sm-10"><span>&copy; blog v2.0 <?php echo date('Y'); ?> </span></div>
+						<div class="caption fw300">Made with <i class="lnr lnr-heart"></i> by <a href="#" target="_blank">Rizki Sukma Wijaya</a></div>
+						<!-- ACROLL TO TOP-->
+						<div class="top-button">
+							<h5>Top</h5>
+							<div class="line"></div>
+						</div>
+					</footer>
+				</section>
